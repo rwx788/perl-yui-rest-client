5444c6192d865cd6f5afc21273740b08ca6e094a
rwx788 <riafarov@suse.com>
2021-05-04 14:43:57 +0200

    Add initial code from os-autoinst-distri-opensuse

---
+++ lib/YuiRestClient.pm
@@ -0,0 +1,21 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Maintainer: QE YaST <qa-sle-yast@suse.de>
+
+package YuiRestClient;
+use strict;
+use warnings;
+
+use constant API_VERSION => 'v1';
+
+use YuiRestClient::App;
+use YuiRestClient::Wait;
+
+1;
